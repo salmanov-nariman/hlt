4d3067ec393ec3776a49769504523d10033fad58
Nariman <narik11102004@gmail.com>
2026-09-14 01:06:16 +0300

    add health check

--- services/auth/cmd/main.go
+++ services/auth/cmd/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func healthCheck(writer http.ResponseWriter, request *http.Request) {
+	log.Println("Пришел запрос на healthCheck")
+	writer.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	log.Println("Сервис auth запущен...")
+
+	http.HandleFunc("/api/auth/healthCheck", healthCheck)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Println("Ошибка при запуске auth сервиса", err)
+	}
+}
