81c909723a54a163ad48d95a812a88c0a3420de7
rustam <abdulkadyrov.rustam@bk.ru>
2026-09-16 11:36:46 +0300

    init worker-service

---
+++ services/worker/cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func healthCheck(writer http.ResponseWriter, request *http.Request) {
+	log.Println("Пришел запрос на healthCheck")
+	writer.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	log.Println("Сервис worker запущен...")
+
+	http.HandleFunc("/api/event/healthCheck", healthCheck)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Println("Ошибка при запуске worker сервиса", err)
+	}
+}
