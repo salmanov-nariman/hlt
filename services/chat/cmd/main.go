c5e887597bcd292068e0c50ebd91de16ffff298c
Nariman <narik11102004@gmail.com>
2026-09-20 20:14:10 +0300

    add grpc client in chat service

--- services/chat/cmd/main.go
+++ services/chat/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"chat/internal/grpc_client/worker"
 	"chat/internal/handler"
 	"chat/internal/service"
 	"chat/internal/websocket"
@@ -9,10 +10,25 @@ import (
 )
 
 func main() {
+	//cfg := config.Load()
+	//
+	//log.Printf("Connecting to Worker Service at %s...", cfg.WorkerAddr)
+	/*
+		КОГДА ВОРКЕР БУДЕТ ГОТОВ В KUBERNETES, РАСКОММЕНТИРУЙ ЭТОТ БЛОК:
+
+		workerAddr := "worker-service:50051"
+		grpcConn, err := grpc.NewClient(workerAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			log.Fatalf("Failed to connect to worker service: %v", err)
+		}
+		defer grpcConn.Close()
+		workerClient := worker.NewGRPCWorkerClient(grpcConn)
+	*/
+
+	workerClient := worker.NewMockWorkerClient()
+
 	wsManager := websocket.NewManager()
-
-	chatService := service.NewChatService(wsManager)
-
+	chatService := service.NewChatService(wsManager, workerClient)
 	chatHandler := handler.NewChatHandler(chatService)
 
 	http.Handle("/", http.FileServer(http.Dir("./frontend")))
--- services/chat/internal/service/chat.go
+++ services/chat/internal/service/chat.go
@@ -1,25 +1,34 @@
 package service
 
 import (
+	"chat/internal/grpc_client/worker"
 	"chat/internal/websocket"
+	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 )
 
 type ChatService struct {
-	wsManager *websocket.Manager
-	// Позже сюда добавятся репозитории для БД и gRPC-клиенты
+	wsManager    *websocket.Manager
+	workerClient worker.WorkerClient
 }
 
-func NewChatService(wsManager *websocket.Manager) *ChatService {
-	return &ChatService{wsManager: wsManager}
+func NewChatService(wsManager *websocket.Manager, workerClient worker.WorkerClient) *ChatService {
+	return &ChatService{
+		wsManager:    wsManager,
+		workerClient: workerClient,
+	}
 }
 
-func (s *ChatService) ProcessMessage(senderID, roomID, text string) error {
+func (s *ChatService) ProcessMessage(ctx context.Context, senderID, roomID, text string) error {
 	log.Printf("[DB] Сохраняем сообщение от %s в БД для чата %s...", senderID, roomID)
 	log.Printf("[gRPC] Отправляем запрос GetDuoMembers(duo_id: %s) в Worker сервис...", roomID)
 
-	targetUserIDs := []string{senderID, "a48e7c82-4b26-4d90-bda7-8b85f3f25abf"}
+	targetUserIDs, err := s.workerClient.GetDuoMembers(ctx, roomID, senderID)
+	if err != nil {
+		return fmt.Errorf("access denied or worker error: %w", err)
+	}
 	log.Printf("[gRPC] Ответ от Worker сервиса получен. Участники: %v", targetUserIDs)
 
 	chatMsg := websocket.ChatMessage{Text: text}
